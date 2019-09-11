bdea1f3a2958acb49e75bdd9a1ff4c4f473b358a
Marie Akhramenko <triangletrue@yahoo.com>
2019-09-11 14:47:41 +0300

    Add convertion of array to image

--- example.py
+++ example.py
@@ -12,6 +12,17 @@ i = Image.open(filename)
 iar = np.asarray(i)
 print(iar)
 
+def random_img(output, width, height):
+  array = np.random.random_integers(0,255, (height,width,3))
+
+  array = np.array(array, dtype=np.uint8)
+  img = Image.fromarray(array)
+  img.save(output)
+  plt.imshow(img)
+  plt.show()
+
+random_img('random.png', 100, 50)
+
 # Create a TensorFlow Variable
 # x = tf.Variable(image, name='x')
 
