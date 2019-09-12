7378c6139c1590dde3e38b9e69323705a312e395
Marie Akhramenko <triangletrue@yahoo.com>
2019-09-12 11:58:58 +0300

    Change values of color of pixel

--- attempt.py
+++ attempt.py
@@ -14,7 +14,6 @@ print(image_array)
 
 def random_img(output, width, height):
   array = np.random.random_integers(0,255, (height,width,3))
-
   array = np.array(array, dtype=np.uint8)
   img = Image.fromarray(array)
   img.save(output)
@@ -35,57 +34,18 @@ tiles = [image_array[x:x+M,y:y+N] for x in range(0,image_array.shape[0],M) for y
 #   img.save(str(i) + '.jpg')
 #   i+=1
 
-# i=0
-# while i<25:
-image = Image.open('1.jpg')
-plt.imshow(image)
-plt.show()
+array_of_x = []
 
-image = Image.open('2.jpg')
-plt.imshow(image)
-plt.show()
+for i, tile in enumerate(tiles):
+  array_of_x.append([])
 
-image = Image.open('3.jpg')
-plt.imshow(image)
-plt.show()
+for i, tile in enumerate(tiles):
+  for j, row in enumerate(tile):
+    for k, pixel in enumerate(row):
+      for p, color in enumerate(pixel):
+        array_of_x[i].append(((2 * color) / 255) - 1)
 
-image = Image.open('4.jpg')
-plt.imshow(image)
-plt.show()
-
-image = Image.open('5.jpg')
-plt.imshow(image)
-plt.show()
-
-image = Image.open('6.jpg')
-plt.imshow(image)
-plt.show()
-
-image = Image.open('7.jpg')
-plt.imshow(image)
-plt.show()
-
-image = Image.open('8.jpg')
-plt.imshow(image)
-plt.show()
-
-image = Image.open('9.jpg')
-plt.imshow(image)
-plt.show()
-
-
-image = Image.open('10.jpg')
-plt.imshow(image)
-plt.show()
-
-image = Image.open(filename)
-plt.imshow(image)
-plt.show()
-
-
-
-plt.show()
-
+print(array_of_x)
 
 # сначала нужно сделать разбиение картинки на маленькие прямоугольники
 # делим на 256 квадратов 16x16 для этого нужно
